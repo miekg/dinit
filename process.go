bc392a93ff8df3e591940b02228486882daecb2f
Miek Gieben <miek@miek.nl>
2015-05-04 09:16:08 +0100

    Add env variables
    Take into account various DINIT_* environment variables.

--- process.go
+++ process.go
@@ -8,21 +8,24 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
 )
 
 var (
+	verbose              bool
 	port, sleep          int
 	namespace, subsystem string
 )
 
 func main() {
-	flag.IntVar(&port, "port", 0, "port to export metricss for Prometheus")
-	flag.IntVar(&sleep, "sleep", 5, "how many seconds to sleep before force killing programs")
-	flag.StringVar(&namespace, "namespace", "", "namespace to use for Prometheus")
-	flag.StringVar(&subsystem, "subsystem", "", "subsystem to use for Prometheus")
+	flag.IntVar(&port, "port", envInt("DINIT_PORT", 0), "port to export metricss for prometheus (DINIT_PORT)")
+	flag.IntVar(&sleep, "sleep", envInt("DINIT_SLEEP", 5), "how many seconds to sleep before force killing programs (DINIT_SLEEP)")
+	flag.StringVar(&namespace, "namespace", envString("DINIT_NAMESPACE", ""), "namespace to use for prometheus (DINIT_NAMESPACE)")
+	flag.StringVar(&subsystem, "subsystem", envString("DINIT_SUBSYSTEM", ""), "subsystem to use for prometheus (DINIT_SUBSYSTEM)")
+	flag.BoolVar(&verbose, "verbose", envBool("DINIT_VERBOSE", false), "be more verbose (DINIT_VERBOSE)")
 
 	flag.Usage = func() {
 		fmt.Fprintln(os.Stderr, "Usage: dinit [OPTION]... PROGRAM [PROGRAM]...")
@@ -52,10 +55,12 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			log.Printf("dinit: pid %d started: %v", cmd.Process.Pid, cmd.Args)
+			if verbose {
+				log.Printf("dinit: pid %d started: %v", cmd.Process.Pid, cmd.Args)
+			}
 
 			err = cmd.Wait()
-			if err != nil {
+			if err != nil && verbose {
 				log.Printf("dinit: pid %d, finished with error: %s", cmd.Process.Pid, err)
 			} else {
 				log.Printf("dinit: pid %d, finished: %v", cmd.Process.Pid, cmd.Args)
@@ -84,7 +89,9 @@ Wait:
 		case sig := <-ints:
 			// There is a race here, because the process could have died, we don't care.
 			for _, cmd := range cmds {
-				log.Printf("dinit: signal %d sent to pid %d", sig, cmd.Process.Pid)
+				if verbose {
+					log.Printf("dinit: signal %d sent to pid %d", sig, cmd.Process.Pid)
+				}
 				cmd.Process.Signal(sig)
 			}
 
@@ -111,7 +118,39 @@ func reaper() {
 		if err != nil {
 			return
 		}
-		log.Printf("dinit: pid %d reaped", pid)
+		if verbose {
+			log.Printf("dinit: pid %d reaped", pid)
+		}
 		zombies.Inc()
 	}
+}
+
+func envBool(k string, d bool) bool {
+	x := os.Getenv(k)
+	switch strings.ToLower(x) {
+	case "true":
+		return true
+	case "false":
+		return false
+	}
+	return d
+
+}
+
+func envInt(k string, d int) int {
+	x := os.Getenv(k)
+	if x != "" {
+		if x1, e := strconv.Atoi(x); e != nil {
+			return x1
+		}
+	}
+	return d
+}
+
+func envString(k, d string) string {
+	x := os.Getenv(k)
+	if x != "" {
+		return x
+	}
+	return d
 }
