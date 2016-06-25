b515f5451480e42592616d7673259ba0ef3ebf22
Miek Gieben <miek@miek.nl>
2016-06-25 22:15:14 +0100

    Fix the arg test after merging #13

--- arg_test.go
+++ arg_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestArgString(t *testing.T) {
-	cmdline := "-r /bin/sleep 10 -r echo -- \\-r"
+	cmdline := "-r /bin/sleep 10 -r /bin/echo -- \\-r"
 	if x := String(Args(strings.Fields(cmdline))); x != cmdline {
 		t.Fatalf("expected '%s', got '%s'", cmdline, x)
 	}
